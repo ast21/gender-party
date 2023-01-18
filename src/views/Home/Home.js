6252dc600e9b222f9ac6d2b19edb846441670a9c
Anastas Mironov <ast@l80.ru>
2023-01-19 02:27:33 +0600

    feat: add timer

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -7,6 +7,28 @@ function Home() {
           <span>важный день</span>
           <span>вы с нами</span>
         </div>
+
+        <div className="timer">
+          <span className="title">До конца голосования осталось</span>
+          <div className="cells">
+            <div className="days">
+              <span className="number">14</span>
+              <span className="title">дней</span>
+            </div>
+            <div className="hours">
+              <span className="number">14</span>
+              <span className="title">часов</span>
+            </div>
+            <div className="minutes">
+              <span className="number">14</span>
+              <span className="title">минут</span>
+            </div>
+            <div className="seconds">
+              <span className="number">14</span>
+              <span className="title">секунд</span>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
